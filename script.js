655fb09f43074b1501ccb895c0bf20a14ab68bb7
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-10-20 01:15:37 +1100

    add slider

--- script.js
+++ script.js
@@ -23,36 +23,38 @@ function fillup(size) {
     for (i = 0; i < 256; i++) {
       const blocks = document.createElement("div");
       blocks.classList.add("sixteen");
-      blocks.addEventListener(
-        "mouseover",
-        () => (blocks.style.backgroundColor = "#" + randomColor)
-      );
       container.append(blocks);
     }
   } else if (size == 32) {
     for (i = 0; i < 1024; i++) {
       const blocks = document.createElement("div");
       blocks.classList.add("thirtytwo");
-      blocks.addEventListener(
-        "mouseover",
-        () => (blocks.style.backgroundColor = "#" + randomColor)
-      );
+
       container.append(blocks);
-    } 
+    }
   } else if (size == 64) {
     for (i = 0; i < 4096; i++) {
       const blocks = document.createElement("div");
       blocks.classList.add("sixtyfour");
-      blocks.addEventListener(
-        "mouseover",
-        () => (blocks.style.backgroundColor = "#" + randomColor)
-      );
       container.append(blocks);
-    } 
+    }
   }
 }
 
-fillup(32);
+
+
+
+function makeBlack(){
+  fillup(16);
+}
+
+const blackbtn = document.getElementById('black')
+blackbtn.addEventListener('click', makeBlack)
+
+// blocks.addEventListener(
+//         "mouseover",
+//         () => (blocks.style.backgroundColor = "#" + randomColor)
+//       );
 
 // function makeItBlack(){
 //    element.style.backgroundColor = "black"
@@ -66,14 +68,31 @@ fillup(32);
 
 const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
-
 // const slider = document.querySelector('.slider-value')
 // function slider() {
 
 // }
 
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', reload)
+//Reset Grid
+const reset = document.getElementById("reset");
+reset.addEventListener("click", reload);
 function reload() {
   window.location.reload();
 }
+
+
+//Slider value
+
+// slider.addEventListener('click',(e)=>console.log(e.target))
+
+// function getValue(){
+//   const slidervalue = document.getElementById('slider-value')
+//   const value = document.getElementById('slider').value;
+//   if (value == 1){
+//     slidervalue.textContent = '2 X 2';
+//   } else if (value == 2){
+//     sliderValue.textContent = '4 X 4';
+//   }
+// }
+
+// getValue();
