4fe736ff95fab733386dc6a874940cffd0f287ec
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-10-19 21:59:08 +1100

    create user interface

--- script.js
+++ script.js
@@ -1,35 +1,79 @@
 const container = document.querySelector(".container");
 
-function makeBlocks(){
-
-}
-
 function fillup(size) {
   if (size == 2) {
     for (i = 0; i < 4; i++) {
       const blocks = document.createElement("div");
-      blocks.classList.add("two-blocks");
+      blocks.classList.add("two");
       container.append(blocks);
     }
   } else if (size == 4) {
     for (i = 0; i < 16; i++) {
       const blocks = document.createElement("div");
-      blocks.classList.add("four-blocks");
+      blocks.classList.add("four");
       container.append(blocks);
     }
   } else if (size == 8) {
     for (i = 0; i < 64; i++) {
       const blocks = document.createElement("div");
-      blocks.classList.add("eight-blocks");
+      blocks.classList.add("eight");
       container.append(blocks);
     }
-  } else if (size == 16){
+  } else if (size == 16) {
     for (i = 0; i < 256; i++) {
       const blocks = document.createElement("div");
-      blocks.classList.add("sixteen-blocks");
+      blocks.classList.add("sixteen");
+      blocks.addEventListener(
+        "mouseover",
+        () => (blocks.style.backgroundColor = "#" + randomColor)
+      );
       container.append(blocks);
     }
+  } else if (size == 32) {
+    for (i = 0; i < 1024; i++) {
+      const blocks = document.createElement("div");
+      blocks.classList.add("thirtytwo");
+      blocks.addEventListener(
+        "mouseover",
+        () => (blocks.style.backgroundColor = "#" + randomColor)
+      );
+      container.append(blocks);
+    } 
+  } else if (size == 64) {
+    for (i = 0; i < 4096; i++) {
+      const blocks = document.createElement("div");
+      blocks.classList.add("sixtyfour");
+      blocks.addEventListener(
+        "mouseover",
+        () => (blocks.style.backgroundColor = "#" + randomColor)
+      );
+      container.append(blocks);
+    } 
   }
 }
 
-fillup(16);
+fillup(32);
+
+// function makeItBlack(){
+//    element.style.backgroundColor = "black"
+// const className = event.target.className;
+//   blocks.classList.remove(className);
+//   blocks.classList.add('.white');
+// }
+
+// const sixteenblocks = document.querySelectorAll('sixteen');
+// sixteenblocks.forEach(block => block.addEventListener('mouseover',() => block.style.backgroundColor = "black");
+
+const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+
+// const slider = document.querySelector('.slider-value')
+// function slider() {
+
+// }
+
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', reload)
+function reload() {
+  window.location.reload();
+}
