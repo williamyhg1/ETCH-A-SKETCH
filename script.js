4e2b1c311b1d8ddfbc974fedd493072974591769
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-10-20 16:59:11 +1100

    modify ranges

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ function fillup(e) {
     for (i = 0; i < 4; i++) {
       const blocks = document.createElement("div");
       blocks.classList.add("block");
+      
       blocks.setAttribute("id", "two");
       blocks.addEventListener(
         "mouseover",
-        () => (blocks.style.backgroundColor = "#" + randomColor)
+        () => (blocks.style.backgroundColor =  "#" + randomColor)
       );
       container.append(blocks);
     }
@@ -19,7 +20,7 @@ function fillup(e) {
       blocks.setAttribute("id", "four");
       blocks.addEventListener(
         "mouseover",
-        () => (blocks.style.backgroundColor = "#" + randomColor)
+        () => (blocks.style.backgroundColor =  "#" + randomColor)
       );
       container.append(blocks);
     }
@@ -30,7 +31,7 @@ function fillup(e) {
       blocks.setAttribute("id", "eight");
       blocks.addEventListener(
         "mouseover",
-        () => (blocks.style.backgroundColor = "#" + randomColor)
+        () => (blocks.style.backgroundColor =  "#" + randomColor)
       );
       container.append(blocks);
     }
@@ -41,7 +42,7 @@ function fillup(e) {
       blocks.setAttribute("id", "sixteen");
       blocks.addEventListener(
         "mouseover",
-        () => (blocks.style.backgroundColor = "black")
+        () => (blocks.style.backgroundColor = "#" + randomColor)
       );
       container.append(blocks);
     }
@@ -63,7 +64,7 @@ function fillup(e) {
       blocks.setAttribute("id", "sixtyfour");
       blocks.addEventListener(
         "mouseover",
-        () => (blocks.style.backgroundColor = "black")
+        () => (blocks.style.backgroundColor = "#" + randomColor)
       );
       container.append(blocks);
     }
@@ -83,8 +84,8 @@ function showSliderValue(e) {
 
 // Remove previous settings
 function removeBlocks() {
-  blocks = document.querySelectorAll(".block");
-  blocks.forEach((item) => item.remove());
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((node) => node.remove());
 }
 
 // Event listners on slider
@@ -93,6 +94,56 @@ slider.addEventListener("mouseup", fillup);
 slider.addEventListener("mousedown", removeBlocks);
 
 const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+//add class "rainbow", "black" and "earse" to the blocks for background manipulation
+const blackbtn = document.getElementById("black");
+const erasebtn = document.getElementById("erase");
+const rainbowbtn = document.getElementById("rainbow");
+
+
+function selectRainbow() {
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((node) => node.classList.remove("black"));
+  blocks.forEach((node) => node.classList.remove("white"));
+  blocks.forEach((node) => node.classList.add("rainbow"));
+}
+
+function selectBlack() {
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((node) => node.classList.remove("white"));
+  blocks.forEach((node) => node.classList.remove("rainbow"));
+  blocks.forEach((node) => node.classList.add("black"));
+}
+
+
+
+function erase() {
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((node) => node.classList.remove("black"));
+  blocks.forEach((node) => node.classList.remove("rainbow"));
+  blocks.forEach((node) => node.classList.add("white"));
+}
+
+rainbowbtn.addEventListener("click", selectRainbow);
+blackbtn.addEventListener("click", selectBlack);
+erasebtn.addEventListener("click", erase);
+
+
+function drawBlack(){
+const blackBlocks = document.querySelectorAll('.black')  
+console.log(blackBlocks[1].target)
+
+// blackBlocks.forEach(item => console.log(item.target))
+}
+
+
+
+// blocks.addEventListener(
+//   "mouseover",
+//   () => (blocks.style.backgroundColor = "#" + randomColor)
+// );
+
+// const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 // const blackbtn = document.getElementById('black')
 // blackbtn.addEventListener('click', makeBlack)
